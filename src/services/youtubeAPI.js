2e45c738b0f0070a13b91d9358b3a98977e40f96
Sai Chandan <Saichandhanyadav2002@gmail.com>
2025-11-10 16:16:06 +0530

    fix: Small fallback fix

--- src/services/youtubeAPI.js
+++ src/services/youtubeAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/youtube/v3";
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
-let useMock = true;
+let useMock = false;
 
 export const fetchFromAPI = async (endpoint) => {
   if (useMock) {
